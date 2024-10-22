658324bcfe0d1b853e2bd973846dafd896ebfc6c
Radeep Yenni <radeeplucky143@gmail.com>
2024-10-22 16:59:08 +0530

    all unit tests added for the delete_object method.

---
+++ src/tests/test_delete_object.py
@@ -0,0 +1,80 @@
+import sys
+import pytest
+from fastapi.testclient import TestClient
+from unittest.mock import patch, MagicMock
+from os.path import dirname, abspath
+from datetime import datetime
+
+sys.path.append(dirname(dirname(abspath(__file__))))
+
+from main import app
+from api.models import DeleteData
+client = TestClient(app)
+
+
+@patch('api.endpoints.DataStore')
+def test_delete_object_success(mock_data_store):
+    """successfully deleeting the record from the database"""
+
+    key = "movie"
+    tenant_id = "radeep"
+    object = DeleteData(tenant_id=tenant_id, key=key)
+    expected_data = {'message': f"Dear {tenant_id},Record {object} deleted successfully."}
+
+    mock_data_store.return_value.tenantExists.return_value = True
+    mock_data_store.return_value.deleteData.return_value = f"Record {object} deleted successfully."
+    mock_data_store.return_value.disconnect.return_value = None
+    response = client.delete(f"/api/object/{key}/{tenant_id}")
+
+    assert response.status_code == 200
+    assert response.json() == expected_data
+
+
+@patch('api.endpoints.DataStore')
+def test_delete_object_user_not_found(mock_data_store):
+    """User Not found case check"""
+
+    key = "movie"
+    tenant_id = "shyam"
+    expected_data = {'message': f'Dear {tenant_id}, you were not registered/exists in the database.'}
+
+    mock_data_store.return_value.tenantExists.return_value = False
+    mock_data_store.return_value.disconnect.return_value = None
+    response = client.delete(f"/api/object/{key}/{tenant_id}")
+
+    assert response.status_code == 404
+    assert response.json() == expected_data
+
+
+@patch('api.endpoints.DataStore')
+def test_delete_object_record_not_found(mock_data_store):
+    """Record not found"""
+
+    key = "movie"
+    tenant_id = "radeep"
+    object = DeleteData(tenant_id=tenant_id, key=key)
+    delete_return = f"Please make sure Record {object} exists in the database before performing delete operation.."
+    expected_data = {'message': f"Dear {tenant_id},{delete_return}"}
+
+    mock_data_store.return_value.tenantExists.return_value = True
+    mock_data_store.return_value.deleteData.return_value = delete_return
+    mock_data_store.return_value.disconnect.return_value = None
+    response = client.delete(f"/api/object/{key}/{tenant_id}")
+
+    assert response.status_code == 200
+    assert response.json() == expected_data
+
+
+@patch('api.endpoints.DataStore')
+def test_delete_object_Exception(mock_data_store):
+    """Unexpected exception case"""
+
+    key = "movie"
+    tenant_id = "shyam"
+    expected_data = {"error":"error occured"}
+
+    mock_data_store.return_value.tenantExists.side_effect = Exception("error occured")
+    response = client.delete(f"/api/object/{key}/{tenant_id}")
+
+    assert response.status_code == 502
+    assert response.json() == expected_data
